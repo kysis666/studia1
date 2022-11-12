68e56148cfa45e660a4d900e3f0577deb5d4e02e
kysis666 <50187331+kysis666@users.noreply.github.com>
2022-11-12 09:52:02 +0100

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Onp.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+
+namespace ConsoleApp1
+{
+    public class OnpClass
+    {
+        public static void Onp(string input)
+        {
+            var stack = new Stack<char>();
+            input = input.Replace(" ", string.Empty);
+            var stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char character = input[i];
+                if (character == '(')
+                    stack.Push(character);
+                else if (character == ')')
+                {
+                    while (stack.Count > 0 && stack.Peek() != '(')
+                        stringBuilder.Append(stack.Pop());
+                    stack.Pop();
+                }
+                else if (IsNum(character))
+                {
+                    stringBuilder.Append(character);
+                }
+                else if (IsOperator(character))
+                {
+                    while (stack.Count > 0 && stack.Peek() != '(' && Prior(character) <= Prior(stack.Peek()))
+                        stringBuilder.Append(stack.Pop());
+                    stack.Push(character);
+                }
+                else
+                {
+                    if (stack.Pop() != '(')
+                        stringBuilder.Append(stack.Pop());
+                }
+            }
+            while (stack.Count > 0)
+            {
+                stringBuilder.Append(stack.Pop());
+            }
+
+            Console.WriteLine(stringBuilder.ToString());
+        }
+        static bool IsOperator(char c)
+        {
+            return (c == '-' || c == '+' || c == '*' || c == '/');
+        }
+        static bool IsNum(char c)
+        {
+            return (c >= '0' && c <= '9' || c == '.');
+        }
+        static int Prior(char c)
+        {
+            switch (c)
+            {
+                case '+':
+                    return 1;
+                case '-':
+                    return 1;
+                case '*':
+                    return 2;
+                case '/':
+                    return 2;
+                default:
+                    throw new Exception("Invalid input");
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using ConsoleApp1;
+using System.Collections;
+
+public class Program : OnpClass
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Stack:");
+        Stack();
+
+        Console.WriteLine("\nQueue:");
+        Queue();
+
+        Console.WriteLine("\nOnp:");
+        Onp("5 + ((1 + 2) * 4) - 3");
+
+    }
+
+    private static void Stack()
+    {
+        Stack stack = new Stack();
+        stack.Push("stack1");
+        stack.Push("stack2");
+        stack.Push("stack3");
+
+        stack.Pop();
+
+        foreach (var item in stack)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    private static void Queue()
+    {
+        Queue queue = new Queue();
+        queue.Enqueue("queue1");
+        queue.Enqueue("queue2");
+        queue.Enqueue("queue3");
+
+        queue.Dequeue();
+
+        foreach (var item in queue)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
